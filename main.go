94be330d817ed13702b6804729300d9da9486cc9
cduyhuynh <cduyhuynh@gmail.com>
2026-02-25 15:45:25 +0700

    add csv file

--- main.go
+++ main.go
@@ -12,6 +12,25 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func whitelist(w http.ResponseWriter, r *http.Request) {
+type BookingParams struct {
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Guests int    `json:"guests"`
+	Date   string `json:"date"`
+	Time   string `json:"time"`
+}
 
+func whitelist(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		methodOptionCORS(w)
+		return
+	}
+}
+
+func methodOptionCORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Max-Age", "3600")
+	w.WriteHeader(http.StatusNoContent)
 }
